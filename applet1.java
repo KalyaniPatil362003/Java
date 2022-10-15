b7e8d69c411419322aafebe1684ef0d2cebd223b
KalyaniPatil362003 <99466290+KalyaniPatil362003@users.noreply.github.com>
2022-10-15 20:43:58 +0530

    Add files via upload

---
+++ applet1.java
@@ -0,0 +1,10 @@
+import java.applet.Applet;
+import java.awt.*;
+public class applet1 extends Applet
+{
+  public void paint(Graphics g)
+  {
+	  g.drawString("fileRect",80,50);
+	  g.feelRect(80,80,60,100);
+	  }
+}
