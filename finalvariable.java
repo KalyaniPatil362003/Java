7317e35999947f6a4e0fa69be5e8d4d9e2b7f5a9
KalyaniPatil362003 <99466290+KalyaniPatil362003@users.noreply.github.com>
2022-10-15 19:05:51 +0530

    Add files via upload

---
+++ finalvariable.java
@@ -0,0 +1,20 @@
+package finalvariable;
+class A
+{
+	final int a;
+	a=10;
+void put()
+{
+	a=a+10;
+	System.out.print("A="+a);
+}
+}
+public class finalvariable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+A o=new A();
+o.put();
+	}
+
+}
